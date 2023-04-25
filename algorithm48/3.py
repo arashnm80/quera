b7e8330e8ae1b44fd08431647afe313d8c7744f3
Arash Nemat Zadeh <Arashnm80@gmail.com>
2023-04-25 23:05:37 +0330

    3.py

--- algorithm48/3.py
+++ algorithm48/3.py
@@ -1,5 +1,26 @@
+def binom(n, k):
+    if k == 1:
+        x = n
+    elif k == 2:
+        x = n * (n - 1) / 2
+    elif k == 3:
+        x = n * (n - 1) * (n - 2) / 3 / 2
+    else:
+        x = 0 # temp
+    
+    if n < k:
+        x = 0
+    return int(x)
+
 T = int(input())
 l = []
 for i in range(T):
     l.append(int(input()))
 
+for n in l:
+    term1 = binom(n, 3) + 2 * binom(n, 2)
+    term2 = 2 * (binom(n, 1) + binom(n, 2))
+    term3 = binom(n, 1)
+    
+    result = term1 + term2 + term3
+    print(result)
